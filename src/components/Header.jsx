e2ee2204da65926e8a84ea47a9060c3cc080b74f
George Armaly <georgear54@gmail.com>
2024-05-27 15:49:45 +0300

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/imgs/logo.svg";
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
