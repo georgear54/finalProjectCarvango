82e8552a2ea9d53c7d1528c3290ef8aa4e4ad9cc
George Armaly <georgear54@gmail.com>
2024-06-03 16:20:23 +0300

    created a main page component with some css

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,12 +5,19 @@ export default function Main() {
     <main className={classes.main}>
       <div>
         <h1>Why Choosing Us?</h1>
+        <p>helloooooooooooooooo</p>
         <p>
-          hello fucker's !!!!
-        </p>
-        <p>
-          Immerse yourself in Specialty Coffee and Irresistible Asian Rolls,
-          Crafted with Lovdwswdcsdcsdce and Passion. Let's "nou(ri)sh" your way!
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Content here, 
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
         </p>
       </div>
 
