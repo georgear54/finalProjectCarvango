08002a259bad927459bf34ae7446c6d4716178c7
Avi1top <abrahemfokra@gmail.com>
2024-06-04 14:47:59 +0300

    menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,14 +4,16 @@ import classes from "./menu.module.css"; // Importing CSS Modules
 
 const MenuPage = () => {
   const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
-      .get("http://localhost:3001/dishes")
+      .get("http://localhost:3001/menu  ")
       .then((response) => {
         setMeals(response.data);
       })
       .catch((error) => {
+        setError("There was an error fetching the meals!");
         console.error("There was an error fetching the meals!", error);
       });
   }, []);
@@ -27,26 +29,37 @@ const MenuPage = () => {
         <h1>Our Menu</h1>
       </header>
       <main className={classes.menuMain}>
-        {meals.map((meal) => (
-          <div key={meal.ID} className={classes.mealItem}>
-            <img
-              src={meal.image}
-              alt={meal.name}
-              className={classes.mealImage}
-            />
-            <div className={classes.mealInfo}>
-              <h2>{meal.name}</h2>
-              <p>{meal.description}</p>
-              <p className={classes.mealPrice}>${meal.price}</p>
-              <button
-                onClick={() => handleAddToCart(meal)}
-                className={classes.buyButton}
-              >
-                Add to Cart
-              </button>
+        {error ? (
+          <div className={classes.error}>{error}</div>
+        ) : (
+          meals.map((meal) => (
+            <div key={meal.ID} className={classes.mealItem}>
+              <img
+                src={meal.image}
+                alt={meal.name}
+                className={classes.mealImage}
+              />
+              <div className={classes.mealInfo}>
+                <h2>{meal.name}</h2>
+                <p>{meal.description}</p>
+                <p className={classes.mealPrice}>${meal.price}</p>
+                <ul>
+                  {meal.ingredients.map((ingredient) => (
+                    <li key={ingredient.ID}>
+                      {ingredient.name}: {ingredient.quantity} {ingredient.unit}
+                    </li>
+                  ))}
+                </ul>
+                <button
+                  onClick={() => handleAddToCart(meal)}
+                  className={classes.buyButton}
+                >
+                  Add to Cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </main>
       <footer className={classes.menuFooter}>
         <p>&copy; 2024 Falafel Food Order System</p>
